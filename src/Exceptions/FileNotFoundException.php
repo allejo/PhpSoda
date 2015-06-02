922c2ebd7090efa6d85faca186689dae38e504a9
allejo <allejo@me.com>
2015-06-02 00:13:53 -0700

    Added FileNotFoundException

---
+++ src/Exceptions/FileNotFoundException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace allejo\Socrata\Exceptions;
+
+class FileNotFoundException extends \Exception
+{
+    public function __construct($filename)
+    {
+        $this->message = "The following file could not be found or opened: " . $filename;
+    }
+}
