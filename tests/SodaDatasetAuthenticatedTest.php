35ec3741bd9a02abf6dd379d3719685cf6acca1c
allejo <allejo@me.com>
2015-06-02 17:56:24 -0700

    Mark replace test as skipped

--- tests/SodaDatasetAuthenticatedTest.php
+++ tests/SodaDatasetAuthenticatedTest.php
@@ -74,6 +74,8 @@ class SodaDatasetAuthenticatedTest extends PHPUnit_Framework_TestCase
 
     public function testReplaceJson ()
     {
+        $this->markTestSkipped("Concurrent writing to a dataset doesn't work");
+
         $ds = new SodaDataset($this->client, $this->id);
 
         $json = file_get_contents("tests/datasets/dataset.json");
