47c505085a8c4af43a58d909ab6de27e6dc66b9c
allejo <vladimir.jimenez.102@my.csun.edu>
2015-06-05 16:15:35 -0700

    Move features from CsvConverter to parent class

--- src/Converters/Converter.php
+++ src/Converters/Converter.php
@@ -2,7 +2,38 @@
 
 namespace allejo\Socrata\Converters;
 
+use allejo\Socrata\Exceptions\FileNotFoundException;
+
 abstract class Converter
 {
+    protected $data;
+
+    public function __construct ($formattedString)
+    {
+        $this->data = $formattedString;
+    }
+
     abstract public function toJson ();
+
+    /**
+     * A convenience method to create a Converter instance from a file name without having to read the file data and
+     * then give it to the CsvConverter constructor.
+     *
+     * @param  string  $filename  The path or filename of the CSV file to open and create a CsvConverter for
+     *
+     * @throws \allejo\Socrata\Exceptions\FileNotFoundException
+     *
+     * @return static
+     */
+    public static function fromFile ($filename)
+    {
+        if (!file_exists($filename) || !is_readable($filename))
+        {
+            throw new FileNotFoundException($filename);
+        }
+
+        $data = file_get_contents($filename);
+
+        return new static($data);
+    }
 }
--- src/Converters/CsvConverter.php
+++ src/Converters/CsvConverter.php
@@ -2,17 +2,8 @@
 
 namespace allejo\Socrata\Converters;
 
-use allejo\Socrata\Exceptions\FileNotFoundException;
-
 class CsvConverter extends Converter
 {
-    private $data;
-
-    public function __construct ($csv)
-    {
-        $this->data = $csv;
-    }
-
     /**
      * Convert the data that was given to this object into JSON.
      *
@@ -32,27 +23,5 @@ class CsvConverter extends Converter
         }
 
         return json_encode($csv);
-    }
-
-    /**
-     * A convenience method to create a CsvConverter instance from a file name without having to read the file data and
-     * then give it to the CsvConverter constructor.
-     *
-     * @param  string  $filename  The path or filename of the CSV file to open and create a CsvConverter for
-     *
-     * @throws \allejo\Socrata\Exceptions\FileNotFoundException
-     *
-     * @return \allejo\Socrata\Converters\CsvConverter
-     */
-    public static function fromFile ($filename)
-    {
-        if (!file_exists($filename) || !is_readable($filename))
-        {
-            throw new FileNotFoundException($filename);
-        }
-
-        $data = file_get_contents($filename);
-
-        return new self($data);
     }
 }
