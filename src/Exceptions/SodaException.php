d724c7fdc49666c76e7466ef3bdfefeb309837cd
allejo <allejo@me.com>
2015-09-23 08:55:13 -0700

    Give access to Socrata's error object

--- src/Exceptions/SodaException.php
+++ src/Exceptions/SodaException.php
@@ -4,9 +4,30 @@ namespace allejo\Socrata\Exceptions;
 
 class SodaException extends \Exception
 {
+    /**
+     * Store the JSON object response when a SODA error is thrown
+     *
+     * @var array
+     */
+    private $jsonResponse;
+
     public function __construct ($jsonResponse)
     {
-        $this->code    = (isset($jsonResponse['code'])) ? $jsonResponse['code'] : 'error.unknown';
-        $this->message = $jsonResponse['message'];
+        $this->jsonResponse = $jsonResponse;
+
+        $this->code    = (isset($this->jsonResponse['code'])) ? $this->jsonResponse['code'] : 'error.unknown';
+        $this->message = $this->jsonResponse['message'];
+    }
+
+    /**
+     * Get an associative array of the error that was thrown by Socrata
+     *
+     * @since 0.1.2
+     *
+     * @return array An associative array of the error Socrata threw
+     */
+    public function getJsonResponse ()
+    {
+        return $this->jsonResponse;
     }
 }
