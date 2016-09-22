1b063a1115ba8919c00f91f4932bbea619b86a10
allejo <allejo@me.com>
2016-09-22 00:35:34 -0700

    Fix unit tests for auth'd API calls

--- tests/SodaDatasetAuthenticatedTest.php
+++ tests/SodaDatasetAuthenticatedTest.php
@@ -18,7 +18,7 @@ class SodaDatasetAuthenticatedTest extends PHPUnit_Framework_TestCase
 
     public function setUp ()
     {
-        $this->id     = "5anq-ef2c";
+        $this->id     = "pwsy-nn4t";
         $this->domain = "opendata.socrata.com";
         $this->token  = "khpKCi1wMz2bwXyMIHfb6ux73";
 
@@ -36,7 +36,7 @@ class SodaDatasetAuthenticatedTest extends PHPUnit_Framework_TestCase
     {
         $ds = new SodaDataset($this->client, $this->id);
 
-        $json = file_get_contents("tests/datasets/dataset.json");
+        $json = file_get_contents(__DIR__ . "/datasets/dataset.json");
 
         $ds->upsert($json);
     }
@@ -44,9 +44,9 @@ class SodaDatasetAuthenticatedTest extends PHPUnit_Framework_TestCase
     public function testUpsertArray ()
     {
         $array = array(
-            array("name" => "Foo Bar", "type" => "Australian"),
-            array("name" => "Qux Baz", "type" => "Book Keeper"),
-            array("name" => "Bon Qaz", "type" => "Telemarketer")
+            array("date" => "2016-01-01", "os" => "Macintosh", "visits" => 10),
+            array("date" => "2016-01-01", "os" => "Windows", "visits" => 20),
+            array("date" => "2016-01-01", "os" => "iOS", "visits" => 30)
         );
 
         $ds = new SodaDataset($this->client, $this->id);
@@ -57,17 +57,16 @@ class SodaDatasetAuthenticatedTest extends PHPUnit_Framework_TestCase
     {
         $ds = new SodaDataset($this->client, $this->id);
 
-        $csvFile = file_get_contents("tests/datasets/dataset.csv");
+        $csvFile = file_get_contents(__DIR__ . "/datasets/dataset.csv");
         $csv = new CsvConverter($csvFile);
 
         $ds->upsert($csv);
     }
 
-    /**
-     * @expectedException \InvalidArgumentException
-     */
     public function testUpsertInvalidData ()
     {
+        $this->expectException(\InvalidArgumentException::class);
+
         $ds = new SodaDataset($this->client, $this->id);
 
         $ds->upsert("muffin and buttons");
@@ -87,12 +86,19 @@ class SodaDatasetAuthenticatedTest extends PHPUnit_Framework_TestCase
         $ds   = new SodaDataset($this->client, $this->id);
         $soql = new SoqlQuery();
 
-        $soql->select(":id,name,type");
+        $soql->select(":id,date,os,visits");
 
         $result = $ds->getDataset($soql);
 
-        $ds->deleteRow($result[0][":id"]);
+        $uID = $result[0][':id'];
 
-        $this->assertCount(2, $ds->getDataset());
+        $ds->deleteRow($uID);
+
+        $soql->select(':id')
+             ->where(':id = ' . $uID);
+
+        $new_result = $ds->getDataset($soql);
+
+        $this->assertEmpty($new_result);
     }
 }
