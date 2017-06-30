1e90741eacfd5bfc88709201d5ca69a4607feb05
allejo <allejo@me.com>
2017-06-29 18:54:54 -0700

    Add bootstrap file for unit tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+
+if (!file_exists(__DIR__ . "/../vendor/autoload.php"))
+{
+    die(
+        "\n[ERROR] You need to run composer before running the test suite.\n".
+        "To do so run the following commands:\n".
+        "    curl -s http://getcomposer.org/installer | php\n".
+        "    php composer.phar install\n\n"
+    );
+}
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$phpunitAliases = [
+    '\PHPUnit\Framework\Test' => '\PHPUnit_Framework_Test',
+    '\PHPUnit\Framework\TestListener' => '\PHPUnit_Framework_TestListener',
+    '\PHPUnit\Framework\Warning' => '\PHPUnit_Framework_Warning',
+    '\PHPUnit\Framework\AssertionFailedError' => '\PHPUnit_Framework_AssertionFailedError',
+    '\PHPUnit\Framework\TestSuite' => '\PHPUnit_Framework_TestSuite',
+];
+
+foreach ($phpunitAliases as $namespaced => $alias) {
+    if (!class_exists($alias)) {
+        class_alias($namespaced, $alias);
+    }
+}
